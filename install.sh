7941a8a262d61f88bfb393726aa058a1cafff691
voydstudios <anthony@anthonycarlson.com>
2022-06-01 22:36:39 -0700

    Edits
    added .gitignore
    Added output to install.sh to inform what is happening
    Updated code in wifi_qrcode.py to use a function and additional lines to the description portion of the script header

--- install.sh
+++ install.sh
@@ -1,3 +1,6 @@
+echo -e "Creating Python Virtual Environment"
 python3 -m venv venv
+
+echo -e "Installing required modules from requirements.txt" 
 source venv/bin/activate
 pip3 install -r requirements.txt
--- wifi_qrcode.py
+++ wifi_qrcode.py
@@ -1,9 +1,15 @@
 # wifi_qrcode.py
 # Version 1.0
 # Written by Anthony Carlson 6/1/2022
+# LinkedIn: https://www.linkedin.com/in/acarlson/
+# Github: https://github.com/voydstudios
 
 import pyqrcode
 from PIL import Image
+
+def main():
+    qr = pyqrcode.create(f'WIFI:S:{ssid};T:{security};P:{password};;')
+    qr.png(output,scale=5)
 
 ssid = input("Enter SSID: ")
 password = input("Enter WiFi Password: ")
@@ -11,5 +17,4 @@ security = input("Security Type (WPA or WEP): ")
 extension=".png"
 output = "".join([ssid,extension])
 
-qr = pyqrcode.create(f'WIFI:S:{ssid};T:{security};P:{password};;')
-qr.png(output,scale=8)
+main()
